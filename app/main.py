50d164464658ed5b5b38e3e35bd0f2a7ecb98f41
SrmxKub <66010853@kmitl.ac.th>
2025-09-20 13:33:45 +0700

    Allow CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.routes import *
 
 app = FastAPI(title="Top Movies Crawler API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # only for dev
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include all routers
 app.include_router(health_router)
